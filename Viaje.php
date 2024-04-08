234b445f38b24a75175a55ba51055d0c459a216f
Rocio-Ayelen-Fernandez <150625389+Rocio-Ayelen-Fernandez@users.noreply.github.com>
2024-04-08 13:10:14 +0000

    Agregado:  Metodos AgregarPasajero, modificarResponsable y verificarPasajero;  menu

--- Viaje.php
+++ Viaje.php
@@ -11,14 +11,16 @@
         private $codigo;
         private $destino;
         private $cantMaxima;
-        private $objPasajeros= [];
+        private $objPasajeros= []; //ARREGLO DE OBJ PASAJERO
+        private $objResponsable;
 
         //METODO CONSTRUCTOR
-        public function __construct($codigoViaje, $destinoViaje, $maxPasajeros, $oPasajeros){
+        public function __construct($codigoViaje, $destinoViaje, $maxPasajeros, $oPasajeros, $oResponsable){
             $this->codigo = $codigoViaje;
             $this->destino = $destinoViaje;
             $this->cantMaxima = $maxPasajeros;
             $this->objPasajeros = $oPasajeros;
+            $this->objResponsable = $oResponsable;
         }
 
         //METODOS DE ACCESO
@@ -35,6 +37,9 @@
         public function getPasajeros(){
             return $this->objPasajeros;
         }
+        public function getResponsable(){
+            return $this->objResponsable;
+        }
 
         //SETTERS
         public function setCodigo($codigoViaje){
@@ -46,40 +51,69 @@
         public function setCantMax($maxPasajeros){
             $this->cantMaxima = $maxPasajeros;
         }
-        public function setPasajeros($aPasajeros){
-            $this->pasajeros = $aPasajeros;
+        public function setPasajeros($oPasajeros){
+            $this->objPasajeros = $oPasajeros;
+        }
+        public function setResponsable($oResponsable){
+            $this->objResponsable = $oResponsable;
+        }
+
+
+        /**
+         * Verifica si el objeto pasajero ya se encuentra en la lista con el DNI ingresado
+         * y devuelve su indice
+         * @param INT $numDni
+         * @return INT
+         */
+        public function verificarPasajero($numDni){
+            $indicePasajero=null;
+            $i=0;
+            do{
+                //($this->getPasajeros()) devuelve el arreglo de pasajeros
+                if( ( ($this->getPasajeros())[$i]->getDni() ) == $numDni ){
+                    $indicePasajero=$i;
+                }
+                $i++;
+            }while( ($indicePasajero == null) && $i < ( count( ($this->getPasajeros()) ) )/*Cantidad de arreglos*/);
+            return $indicePasajero;
         }
 
         /**
-         * Con el DNI del pasajero se verifica que el pasajero este en el viaje y se modifican sus atributos
-         * @param INT $numDni
-         * @param STRING $nuevoNombre
-         * @param STRING $nuevoApellido
-         * @param STRING $nuevoTelefono
-         * @return STRING
+         * Agrega a un objetoPasajero al arreglo de pasajeros en la clase 
+         * y devuelve un boolean dependiendo la situacion 
+         * @param OBJ $nuevoPasajero
+         * @return BOOLEAN
          */
-        public function ModificarPasajero($numDni, $nuevoNombre, $nuevoApellido, $nuevoTelefono){
-            $mensaje="";
-            $pasajeroEncontrado=false;
-            $i=0;
-            do{
-
-                //($this->getPasajeros()) devuelve el arreglo de pasajeros
+        public function agregarPasajero($nuevoPasajero){
+            $estado = false;
+            $nuevoArreglo=$this->getPasajeros();
+            $verificacion = $this->verificarPasajero($nuevoPasajero->getDni());
+            echo "Verificacion es: ".$verificacion."\n";
+            echo "La cnatidad de arreglos es".count($nuevoArreglo)."\n";
+            echo "Cantidad maxima de array es: ".$this->getCantMax()."\n";
+            if( ($verificacion) == null && count($nuevoArreglo) < $this->getCantMax()  ){
+                $nuevoArreglo[ count($nuevoArreglo) ] = $nuevoPasajero;
                 
-                if( ($this->getPasajeros())[$i]->getDni() == $numDni){
-                    $pasajeroEncontrado=true;
-                    ($this->getPasajeros())[$i]->setNombre($nuevoNombre);
-                    ($this->getPasajeros())[$i]->setApellido($nuevoApellido);
-                    ($this->getPasajeros())[$i]->setTelefono($nuevoTelefono);
-                }
-                $i++;
-            }while(!$pasajeroEncontrado);
-
-            if($pasajeroEncontrado){
-                $mensaje="Se ha encontrado el pasajero  ha sido modificada su informacion";
+                $this->setPasajeros($nuevoArreglo);
+                
+                $estado=true;
             }
+            return $estado;
+        }
 
-
+        /**
+         * Verifica que el responsable ingresado este en el viaje y modifica sus datos segun 
+         */
+        public function modificarResponsable($nEmpleado, $nuevoNombre, $nuevoApellido, $nuevaLicencia){
+            $estado = false;
+            if (($this->getResponsable())->getEmpleado() == $nEmpleado){
+                $estado = true;
+                
+                ($this->getResponsable())->setNombre($nuevoNombre);
+                ($this->getResponsable())->setApellido($nuevoApellido);
+                ($this->getResponsable())->setLicencia($nuevaLicencia); 
+            }
+            return $estado;
         }
 
 
@@ -87,14 +121,17 @@
         public function __toString(){
             $pasajeros="";
             foreach ($this->getPasajeros() as $persona) {
-                $pasajeros = $pasajeros .$persona->getNombre()." ".$persona->getApellido()." ".$persona->getTelefono()." ".$persona->getDni().", ";
+                $pasajeros = $pasajeros .$persona .", ";
             }
 
             return "Codigo de Viaje: ".$this->getCodigo().", Destino: ".$this->getDestino()
-            .", Maximo de pasajeros: ".$this->getCantMax().", Pasajeros: ".$pasajeros;
+            .", Maximo de pasajeros: ".$this->getCantMax().", Pasajeros: ".$pasajeros."Responsable: ".$this->getResponsable();
         }
 
     }
 
+
+
+    
 
 ?>
--- testViaje.php
+++ testViaje.php
@@ -2,6 +2,7 @@
 
     include 'Viaje.php';
     include 'Pasajero.php';
+    include 'ResponsableV.php';
 
     //ARREGLOS DE PASAJEROS
     $objP1= new Pasajero("Juan", "Ramos", 2991822372, 39517428);
@@ -11,23 +12,212 @@
 
     $arregloPasajeros = [$objP1, $objP2, $objP3, $objP4];
 
-    //OBJETO VIAJE (EJ: 3682, "Buenos Aires", 10, $arregloPasajeros )
-    $objViaje = new Viaje(3682, "Buenos Aires", 10, $arregloPasajeros);
+    
+
+
+    //OBJETO VIAJE (EJ: 3682, "Buenos Aires", 10, $arregloPasajeros, $objResponsable )
+    $objViaje = null;
+    $objResponsable = null;
 
     //VARIABLES
     $opcion=0;
     $valorIng="";
-
+    $opcionMod =0;
+    $codigo="";
+    $destino="";
+    $limite="";
+    $nombre ="";
+    $apellido ="";
+    $nEmpleado="";
+    $nLicencia="";
+    $telefono="";
     
-    $objViaje->ModificarPasajero(22812370, "XLR8", "Diez", "454677986");
-    echo $objViaje."\n";
     
-    /*
+    
     echo "Bienvenido al sistema\n";
-    echo "Elija una opcion";
-    echo "\n1. Cargar un viaje\n2. Modificar codigo de viaje\n3. Modificar destino\n4. Modificar cantidad maxima de pasajeros\n5. Cargar Nuevo pasajero\n6. Ver datos\n7. Salir\n";
+    echo "Elija una opcion\n";
+    echo "1. Cargar un viaje\n2. Modificar o agregar datos de viaje\n3. Salir";
     $opcion = trim(fgets(STDIN));
-    while($opcion!=7){
+
+    while($opcion!=3){
+
+        switch($opcion){
+            case 1:
+                echo "Ingrese el codigo de viaje: ";
+                $codigo = trim(fgets(STDIN));
+
+                echo "Ingrese el destino: ";
+                $destino = trim(fgets(STDIN));
+
+                echo "Ingrese el limite de pasajeros: ";
+                $limite = trim(fgets(STDIN));
+
+                echo "Ingrese los datos del responsable del viaje\n";
+                echo "Ingrese el nombre: ";
+                $nombre = trim(fgets(STDIN));
+
+                echo "Ingrese el apellido: ";
+                $apellido = trim(fgets(STDIN));
+
+                echo "Ingrese el numero de empleado: ";
+                $nEmpleado = trim(fgets(STDIN));
+
+                echo "Ingrese el numero de licencia: ";
+                $nLicencia = trim(fgets(STDIN));
+
+                //OBJETO RESPONSABLE
+                $objResponsable = new ResponsableV($nombre, $apellido, $nEmpleado, $nLicencia);
+
+                echo "Se cargaran pasajeros por defecto\n";
+
+                //OBJETO VIAJE
+
+                $objViaje= new Viaje($codigo, $destino, $limite, $arregloPasajeros, $objResponsable);
+
+                break;
+            case 2:
+                if($objViaje === null){
+                    echo "No existe una clase Viaje para modificar\n";
+                }else{
+
+                    echo "1. Modificar datos del viaje\n2. Cargar Nuevo pasajero\n3. Modificar Pasajero\n4. Modificar Responsable de Viaje\n5. Ver datos\n6. Salir\n";
+                    $opcionMod = trim(fgets(STDIN));
+
+                    while($opcionMod != 6){
+                        switch ($opcionMod) {
+                            case 1:
+                                echo "Modificar datos del viaje\n";
+
+                                echo "Ingrese el codigo de viaje: ";
+                                $valorIng = trim(fgets(STDIN));
+                                $objViaje->setCodigo($valorIng);
+
+                                echo "Ingrese el destino: ";
+                                $valorIng = trim(fgets(STDIN));
+                                $objViaje->setDestino($valorIng);
+    
+                                echo "Ingrese el limite de pasajeros: ";
+                                $valorIng = trim(fgets(STDIN));
+                                $objViaje->setCantMax($valorIng);
+    
+                                break;
+                            case 2:
+                                echo "Cargar Nuevo Pasajero\n";
+
+                                echo "Ingrese los datos del pasajero\n";
+                                echo "Ingrese el nombre: ";
+                                $nombre = trim(fgets(STDIN));
+                
+                                echo "Ingrese el apellido: ";
+                                $apellido = trim(fgets(STDIN));
+
+                                echo "Ingrese el telefono del pasajero: ";
+                                $telefono = trim(fgets(STDIN));
+
+                                echo "Ingrese el DNI del pasajero: ";
+                                $dni= trim(fgets(STDIN));
+
+                                $nuevoPasajero= new Pasajero($nombre, $apellido, $telefono, $dni);
+                                
+                                if($objViaje->agregarPasajero($nuevoPasajero)){
+                                    echo "Pasajero ingresado a la lista\n";
+                                }else{
+                                    echo "El pasajero no se ha podido ingresar (Ya esta en la lista o limite alcanzado)\n";
+                                }
+                                break;
+                            case 3:
+                                echo "Modificar Pasajero\n";
+
+                                echo "Ingrese el DNI del pasajero que dese modificar";
+                                $dni = trim(fgets(STDIN));
+                                $verificacion = $objViaje->verificarPasajero($dni);
+                                if($verificacion != null){
+                                    echo "Se ha encontrado el pasajero,  los datos.\n";
+                                    echo "Proceda a ingresar los datos a modificar\n";
+                                    echo "Ingrese el nombre: ";
+                                    $nombre = trim(fgets(STDIN));
+                                    $objViaje->getPasajeros()[$verificacion]->setNombre($nombre);
+
+                                    echo "Ingrese el apellido: ";
+                                    $apellido = trim(fgets(STDIN));
+                                    $objViaje->getPasajeros()[$verificacion]->setApellido($apellido);
+
+                                    echo "Ingrese el telefono del pasajero: ";
+                                    $telefono = trim(fgets(STDIN));
+                                    $objViaje->getPasajeros()[$verificacion]->setTelefono($telefono);
+
+                                    
+                                }else{
+                                    echo "El pasajero no fue encontrado en la lista\n";
+                                }
+
+                                break;
+                            case 4:
+
+                                echo "Modificar Responsable de Viaje\n";
+
+                                echo "Ingrese el numero de empleado del responsable a modificar: ";
+                                $nEmpleado = trim(fgets(STDIN));
+
+                                if($objViaje->getResponsable()->getEmpleado() == $nEmpleado ){
+
+                                    echo "Empleado encontrado\n";
+                                    echo "Ingrese los datos a modificar\n";
+                                    
+                                    echo "Ingrese el nombre: ";
+                                    $nombre = trim(fgets(STDIN));
+                                    ($objViaje->getResponsable())->setNombre($nombre);
+
+                                    echo "Ingrese el apellido: ";
+                                    $apellido = trim(fgets(STDIN));
+                                    ($objViaje->getResponsable())->setApellido($apellido);
+
+                                    echo "Ingrese la licencia: ";
+                                    $nlicencia = trim(fgets(STDIN));
+                                    ($objViaje->getResponsable())->setLicencia($nLicencia);                                    
+                                     
+                                }else{
+                                    echo "No se ha encontrado el empleado\n";
+                                }
+
+                                break;
+                            case 5:
+                                echo "Ver datos\n";
+
+                                echo $objViaje."\n";
+                                break;
+                            default:
+                            echo "La opcion elegida no se encuentra disponible\n";
+                                break;
+    
+                        }
+
+                        echo "1. Modificar datos del viaje\n2. Cargar Nuevo pasajero\n3. Modificar Pasajero\n4. Modificar Responsable de Viaje\n5. Ver datos\n6. Salir\n";
+                        $opcionMod = trim(fgets(STDIN));
+
+
+                    }
+
+                }
+
+                break;
+            default:
+                echo "La opcion elegida no se encuentra disponible\n";
+                break;    
+        }
+
+        echo "Elija una opcion\n";
+        echo "1. Cargar un viaje\n2. Modificar o agregar datos de viaje\n3. Salir";
+        $opcion = trim(fgets(STDIN));
+    }
+
+
+
+
+    /*
+    echo "1. Modificar datos del viaje\n2. Cargar Nuevo pasajero\n3. Modificar Pasajero\n4. Modificar Responsable de Viaje\n5. Ver datos\n6. Salir\n";
+    $opcion = trim(fgets(STDIN));
+    while($opcion!=3){
         
         
         switch ($opcion) {
@@ -104,7 +294,6 @@
         echo "\n1. Cargar un viaje\n2. Modificar codigo de viaje\n3. Modificar destino\n4. Modificar cantidad maxima de pasajeros\n5. Cargar Nuevo pasajero\n6. Ver datos\n7. Salir\n";
         $opcion= trim(fgets(STDIN));   
     }
-
-    */
     
+    */
 ?>
